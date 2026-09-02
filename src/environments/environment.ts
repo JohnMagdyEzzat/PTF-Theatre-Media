74a1db8c23a5baa211c26a522781db2caeab6214
John Magdy <j.magdy18@yahoo.com>
2026-09-02 21:46:35 +0300

    update environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// environment.ts
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://onzhqeumxmuqbsfmyikf.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uemhxZXVteG11cWJzZm15aWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzMzc0MTgsImV4cCI6MjEwMzkxMzQxOH0.g_I4sZpMkfXwL3Fqzt8-d9WPzxkGT8Kx67Vc3gLvyjI',
+};
